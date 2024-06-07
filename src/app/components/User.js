db03f571cca2dcdcef4855579febc02ed3d3f672
Anurag Singh Solanki <90393527+LostAnurag@users.noreply.github.com>
2024-06-07 16:38:26 +0530

    Update User.js

--- src/app/components/User.js
+++ src/app/components/User.js
@@ -1,56 +1,64 @@
 "use client"
 import React, { useEffect, useState } from 'react'
-
+import axios from 'axios'
 export default function User() {
 
-  const userData = [
-    {
-      username: 'Badmosh',
-      id: 15,
-      email: 'example@gmail.com'
-    },
-    {
-      username: 'JohnDoe',
-      id: 27,
-      email: 'johndoe@example.com'
-    },
-    {
-      username: 'AliceSmith',
-      id: 42,
-      email: 'alice.smith@example.com'
-    },
-    {
-      username: 'Anurag',
-      id: 22,
-      email: 'alice.smith@example.com'
-    }
-  ];
+  // const userData = [
+  //   {
+  //     username: 'Badmosh',
+  //     id: 15,
+  //     email: 'example@gmail.com'
+  //   },
+  //   {
+  //     username: 'JohnDoe',
+  //     id: 27,
+  //     email: 'johndoe@example.com'
+  //   },
+  //   {
+  //     username: 'AliceSmith',
+  //     id: 42,
+  //     email: 'alice.smith@example.com'
+  //   },
+  //   {
+  //     username: 'Anurag',
+  //     id: 22,
+  //     email: 'alice.smith@example.com'
+  //   }
+  // ];
+
+  // get - fetch / data read
+  // patch - data / update
+  // delete / delete
+  // post / post new data in api
 
   const [user, setUser] = useState([])
 
   useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+        setUser(response.data); // Assuming the response data contains user data
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
 
-    const applyUser = () => {
-      setUser(userData)
-    } //function
+    fetchUser();
+  }, []); // Empty dependency array means this effect runs only once, on component mount
 
-    applyUser() // function call
+  console.log(user)
 
-  }, [])
 
-  
-  const filteredUsers = user.filter(response => response.id == 42)
- 
-  
+
   return (
     <>
-    {filteredUsers.map((response, index) => (
-      <div key={index} className='bg-gray-200'>
-        <div className='text-2xl text-[#ff0000]'>Username:{response.username}</div>
-        <div>ID:{response.id}</div>
-        <div>Email:{response.email}</div>
-      </div>
-    ))}
+      {user.map((response, index) => (
+        <div key={index} className='bg-gray-200'>
+          <div className='text-2xl text-[#ff0000]'>Username: {response.userId}</div>
+          <div>ID: {response.id}</div>
+          <div>Title: {response.title}</div>
+        </div>
+      ))}
     </>
   )
 }
